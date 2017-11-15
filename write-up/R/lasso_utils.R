f86631355554c595559b50b891643e6adb3fed72
Runjing <runjing_liu@berkeley.edu>
2017-11-14 19:28:31 -0800

    logistic reg utils

--- write-up/R/lasso_utils.R
+++ write-up/R/lasso_utils.R
@@ -1,3 +1,50 @@
-get_logistic_fit <- function(training_set, smooth_features = TRUE){
+get_logistic_fit <- function(image, smooth_features = FALSE){
+  # takes an image that is the output of prep_image
+  # and returns a logistic regression fit
   
+  labeled_indx <- which(image$label != 0)
+  labeled_image <- image[labeled_indx, ]
+  
+  y <- droplevels(labeled_image$label)
+  
+  if(smooth_features){
+    X <- image[, grep("_smoothed", colnames(image))] # get smoothed features
+  }
+  else{
+    feature_names <- c('NDAI','SD','CORR','DF','CF','BF','AF','AN')
+    X <- image[, feature_names]
+  }
+  
+  data <- cbind(y, X)
+  
+  fit <- glm(y ~ ., data = data, family = 'binomial')
+  
+  return(fit)
+}
+
+get_logistic_prediction <- function(fit, image_test){
+  # takes in a logistic fit, a new image and predicts on that image
+  
+  # check if the fit was on smoothed or nonsmoothed features
+  smooth_features <- 'NDAI_smoothed' %in% names(fit$coefficients)
+  # else the feature would be called 'NDAI' without the smoothed
+  
+  labeled_indx <- which(image_test$label != 0)
+  labeled_image <- image_test[labeled_indx, ]
+  
+  y <- droplevels(labeled_image$label)
+  
+  if(smooth_features){
+    X <- image_test[, grep("_smoothed", colnames(image))] # get smoothed features
+  } else{
+    feature_names <- c('NDAI','SD','CORR','DF','CF','BF','AF','AN')
+    X <- image[, feature_names]
+  }
+  
+  data <- cbind(y, X)
+  y_pred <- predict(fit, 
+                newdata = data,
+                type = 'response')
+  
+  return(y_pred)
 }
