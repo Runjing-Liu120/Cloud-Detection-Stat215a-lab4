0b186dd89f40124cf0e82610cdaee3b525b3fee4
NanYang <nanyang@berkeley.edu>
2017-11-15 10:55:48 -0800

    modified rf.R

--- write-up/R/rf.R
+++ write-up/R/rf.R
@@ -4,10 +4,38 @@ library(glmnet)
 library(ranger)
 library(dplyr)
 
+prep_image_no_scaling <- function(filename = 'image1.txt', path = 'image_data/'){
+  #This function takes in the image file name and path and 
+  #returns a dataframe including the original columns with 
+  #additional columns representing each feature smoothed. It also
+  #preps the dataframe for the models by making the labels into factors
+  #and getting rid of the observations with labeled "0".
+  
+  collabs <- c('y','x','label','NDAI','SD','CORR','DF','CF','BF','AF','AN')
+  
+  # Reading-in image, setting column names
+  image <- read.table(paste0(path, filename), header = F)
+  names(image) <- collabs
+  
+  # Adding columns of smoothed features
+  image.features <- image[, -c(1,2,3)]
+  image.labels <- image[, 3]
+  image.added <- smooth_all_features(image.features,
+                                     coord = image[, 1:2])
+  image <- cbind(image[1:3], image.added)
+  
+  # Getting rid of zero labels and making them factors
+  image$label <- as.factor(image$label)
+  image <- image %>%
+    filter(label != 0)
+  
+  return(image)
+}
+
 get_rf_fit <- function(image, smooth_features = FALSE){
   labeled_indx <- which(image$label != 0)
-  labeled_image <- image[labeled_indx, ]
-  y <- droplevels(labeled_image$label)
+  image <- image[labeled_indx, ]
+  y <- droplevels(image$label)
   
   if(smooth_features){
     X <- image[, grep("_smoothed", colnames(image))] # get smoothed features
@@ -28,9 +56,9 @@ get_rf_fit <- function(image, smooth_features = FALSE){
 get_rf_prediction <- function(model, image){
   smooth_features <- 'NDAI_smoothed' %in% names(model$variable.importance)
   labeled_indx <- which(image$label != 0)
-  labeled_image <- image[labeled_indx, ]
+  image <- image[labeled_indx, ]
   
-  y <- droplevels(labeled_image$label)
+  y <- droplevels(image$label)
   
   if(smooth_features){
     X <- image[, grep("_smoothed", colnames(image))] # get smoothed features
@@ -59,7 +87,6 @@ get_labels_from_prob <- function(probabilities, thresh_lb = 0.4, thresh_ub = 0.6
     -1.0 * (probabilities < thresh_lb) 
   
   return(labels)
-  
 }
 
 
