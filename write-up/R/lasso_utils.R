eee5949ec27af48b55cb02493dbe38ac4ae0590e
Runjing <runjing_liu@berkeley.edu>
2017-11-14 20:10:47 -0800

    logistic reg results

--- write-up/R/lasso_utils.R
+++ write-up/R/lasso_utils.R
@@ -24,10 +24,12 @@ get_logistic_fit <- function(image, smooth_features = FALSE){
 
 get_logistic_prediction <- function(fit, image_test){
   # takes in a logistic fit, a new image and predicts on that image
+  # returns predicted probabilities, the predicted class labels, and the
+  # predictive accuracy
   
   # check if the fit was on smoothed or nonsmoothed features
   smooth_features <- 'NDAI_smoothed' %in% names(fit$coefficients)
-  # else the feature would be called 'NDAI' without the smoothed
+  # else the feature would be called 'NDAI' without the '_smoothed'
   
   labeled_indx <- which(image_test$label != 0)
   labeled_image <- image_test[labeled_indx, ]
@@ -35,16 +37,24 @@ get_logistic_prediction <- function(fit, image_test){
   y <- droplevels(labeled_image$label)
   
   if(smooth_features){
-    X <- image_test[, grep("_smoothed", colnames(image))] # get smoothed features
+    X <- image_test[, grep("_smoothed", colnames(image_test))] # get smoothed features
   } else{
     feature_names <- c('NDAI','SD','CORR','DF','CF','BF','AF','AN')
-    X <- image[, feature_names]
+    X <- image_test[, feature_names]
   }
   
   data <- cbind(y, X)
-  y_pred <- predict(fit, 
-                newdata = data,
-                type = 'response')
+  y_pred_prob <- predict(fit, 
+                newdata = X,
+                type = 'response') # returns a probability of being +1 (vs. -1)
   
-  return(y_pred)
+  # get the class label from probability
+  y_pred_class <- 1.0 * (y_pred_prob > 0.5) + (-1.0) * (y_pred_prob < 0.5)
+  
+  # get predictive accuracy
+  pred_accuracy <- mean(y_pred_class == y)
+  
+  return(list(pred_prob = y_pred_prob, 
+              pred_class = y_pred_class, 
+              accuracy = pred_accuracy))
 }
